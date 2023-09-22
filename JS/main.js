9062a429af313ee3aaa3bcaf7db2b10b852b8f08
damiru003 <gamagedamiru@gmail.com>
2023-09-23 00:16:05 +0530

    connect js

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+console.log("Hello world !");
+//var,let,const
+var name="Damiru";
+console.log(name+" is my name.");
+
+//function=method
+function print(){
+
+    console.log("print called...");
+}
+
+//function calling
+print();
+
+
+function getData(){
+    var email=document.getElementById('email').value;
+    console.log(email);
+}
